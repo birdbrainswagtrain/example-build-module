bfef39c803df25b2bdd9ec09e9bd9d9e39056968
Adam Coggeshall <dameerkat@gmail.com>
2015-01-11 15:19:20 -0700

    Quick testing

--- premake5.lua
+++ premake5.lua
@@ -14,5 +14,6 @@ LINUX()
 
 PROJECT()
 SOURCE_SDK_LINKS()
+
 configuration 		"windows"
-configuration 		"linux"
+	postbuildcommands{"move /Y ..\\..\\bin\\gm_".._SOLUTION_NAME..'_win32.dll "%ProgramFiles(x86)%\\Steam\\steamapps\\common\\GarrysMod\\garrysmod\\lua\\bin\\gmsv_'.._SOLUTION_NAME..'_win32.dll"'}
